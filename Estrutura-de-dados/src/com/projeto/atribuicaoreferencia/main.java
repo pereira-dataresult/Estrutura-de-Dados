aa14c6ec274ef9251b2b13736282ce6b2f3aa84f
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-09 21:54:07 -0300

    Subindo o repositorio

---
+++ Estrutura-de-dados/src/com/projeto/atribuicaoreferencia/main.java
@@ -0,0 +1,22 @@
+package com.projeto.atribuicaoreferencia;
+
+public class main {
+    public static void main(String[] args) {
+
+        int intA = 1;
+        int intB = intA;
+        System.out.println("IntA = " + intA + " IntB = " + intB);
+        intA = 2;
+        System.out.println("IntA = " + intA + " IntB = " + intB);
+
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA;
+
+        System.out.println("objA = " + objA + " objB = " + objB);
+
+        objA.setNum(2);
+        System.out.println("objA = " + objA + " objB = " + objB);
+
+
+    }
+}
