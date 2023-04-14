c66a444674c98f8629b17af1b0f9f4bce8733874
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-13 22:03:26 -0300

    subindo arquivos

---
+++ programacao-orientada-objetos/src/com/dio/Inteface/operacaomatematica.java
@@ -0,0 +1,12 @@
+package com.dio.Inteface;
+
+interface operacaomatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+}
